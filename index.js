6d79cd644b6976761a8c3e7fd93d2f15023afde5
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-07-16 21:42:57 +0100

    fix:update imports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const redis = require("redis");
-const fetch = require("node-fetch");
+import express from "express";
+import redis from "redis";
+import fetch from "node-fetch";
 
 const username = "username";
 const PORT = 3000;
@@ -8,6 +8,33 @@ const REDIS_PORT = 6379;
 
 const client = redis.createClient(REDIS_PORT);
 const app = express();
+
+//Forat Output Middleware
+async function formatOutput(username, numRepos) {
+  return `${username} has ${numRepos} repos`;
+}
+
+//Get Repos from Github
+async function getRepos(req, res) {
+  try {
+    const username = req.params[USER_NAME];
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    console.log(response.json());
+
+    const { public_repos } = response.json();
+
+    //Set data to redis
+    client.set(username, public_repos);
+
+    res.send(formatOutput(username, data));
+  } catch (err) {
+    console.log(err);
+
+    res.send(500);
+  }
+}
 
 //Cache Middleware
 function cache(req, res, next) {
@@ -17,10 +44,14 @@ function cache(req, res, next) {
     if (err) throw err;
 
     if (data !== null) {
-      res.send();
+      res.send(formatOutput(username, data));
+    } else {
+      next();
     }
   });
 }
+
+app.get("/repos/:username", cache, getRepos);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
